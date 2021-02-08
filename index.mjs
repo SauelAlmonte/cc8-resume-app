c99732f1572a631dec6356053a895870ac94798d
msimbo-coding <SAlmonte@MSIMBO.org>
2021-02-08 04:29:54 -0500

    added tag, pushed to npm

--- index.mjs
+++ index.mjs
@@ -14,33 +14,47 @@ const { details : { education : { school2, school3, certificate2, certificate3 }
 
 const { details : { interests : { reading : { book1, book2, book3 } } } } = resume
 
-const { details : { interests : { reading : { author1, author2, author3 } } } } = resume
+// const { details : { interests : { reading : { author1, author2, author3 } } } } = resume
 
-const { details : { interests : { sports : { basketball, baseball, football } } } } = resume
+// const { details : { interests : { sports : { basketball, baseball, football } } } } = resume
+
+// \n makes space in console.log
+
+console.log(`
+    =======
+    FName: ${firstName} \n
+    LName: ${lastName} \n
+    Email: ${email} \n
+    Number: ${number}\n
+    Location: ${city}, ${state}. \n
+    Education: ${school1}, ${school2}, & ${school3} \n
+    Certificates: ${certificate1}, ${certificate2} & ${certificate3} \n
+    Interests: I like reading favorite books, ${book1}, ${book2} & ${book3}. I watch a lot of sports 
+`)
 
 
-console.log(`Hello I'm ${firstName} ${lastName}`)
+// console.log(`Hello I'm ${firstName} ${lastName}`)
 
-console.log(`================`)
+// console.log(`================`)
 
-console.log(`You may contact me at ${number} or ${email}`)
+// console.log(`You may contact me at ${number} or ${email}`)
 
-console.log(`================`)
+// console.log(`================`)
 
-console.log(`I'm from ${city} ${state}.`)
+// console.log(`I'm from ${city} ${state}.`)
 
-console.log(`================`)
+// console.log(`================`)
 
-console.log(`I have attended ${school1}, and became certified in ${certificate1}`)
+// console.log(`I have attended ${school1}, and became certified in ${certificate1}`)
 
-console.log(`================`)
+// console.log(`================`)
 
-console.log(`I've also attended ${school2} and received my certification in ${certificate2}, as well as ${school3} for ${certificate3}`)
+// console.log(`I've also attended ${school2} and received my certification in ${certificate2}, as well as ${school3} for ${certificate3}`)
 
-console.log(`================`)
+// console.log(`================`)
 
-console.log(`My favorite books are, ${book1} by ${author1}, ${book2} by ${author2} & ${book3} by ${author3}`)
+// console.log(`My favorite books are, ${book1} by ${author1}, ${book2} by ${author2} & ${book3} by ${author3}`)
 
-console.log(`================`)
+// console.log(`================`)
 
-console.log(`As for sports??? ${basketball}, ${baseball}, & The ${football}. I am from Massachusetts, I always root for the Home Team!`)
+// console.log(`As for sports??? ${basketball}, ${baseball}, & The ${football}. I am from Massachusetts, I always root for the Home Team!`)
