99a81ca6a8807385b9aa3ec3f6e34eec219d703e
msimbo-coding <SAlmonte@MSIMBO.org>
2021-02-08 02:59:42 -0500

    updating resume

--- index.mjs
+++ index.mjs
@@ -1,19 +1,46 @@
+#!/usr/bin/env node
+
 import { resume }  from "./resume.mjs";
 
-// const { location, name:{fName, lName} } = resume;
+const { details : { firstName, lastName} } = resume
 
-const { name, location:{city, state} } = resume;
+const { details : { contact : {number, email} } } = resume
 
-const { schools:{from, certificate} } = resume;
+const { details : { location : {city, state} } } = resume
 
-console.log(`Hello I'm ${name.fName} ${name.lName}`);
+const { details : { education : { school1, certificate1 } } } = resume
 
-console.log(`----------------`);
+const { details : { education : { school2, school3, certificate2, certificate3 } } } = resume
 
-console.log(`I'm from ${city} ${state}.`);
+const { details : { interests : { reading : { book1, book2, book3 } } } } = resume
 
-console.log(`----------------`);
+const { details : { interests : { reading : { author1, author2, author3 } } } } = resume
 
-console.log(`I have attended ${from}, and became certified in ${certificate}.`);
+const { details : { interests : { sports : { basketball, baseball, football } } } } = resume
 
-// console.log(`for ${newEngArtInst.second} and ${berkCollMusic.second}`)
+
+console.log(`Hello I'm ${firstName} ${lastName}`)
+
+console.log(`================`)
+
+console.log(`You may contact me at ${number} or ${email}`)
+
+console.log(`================`)
+
+console.log(`I'm from ${city} ${state}.`)
+
+console.log(`================`)
+
+console.log(`I have attended ${school1}, and became certified in ${certificate1}`)
+
+console.log(`================`)
+
+console.log(`I've also attended ${school2} and received my certification in ${certificate2}, as well as ${school3} for ${certificate3}`)
+
+console.log(`================`)
+
+console.log(`My favorite books are, ${book1} by ${author1}, ${book2} by ${author2} & ${book3} by ${author3}`)
+
+console.log(`================`)
+
+console.log(`As for sports??? ${basketball}, ${baseball}, & The ${football}. I am from Massachusetts, I always root for the Home Team!`)
--- resume.mjs
+++ resume.mjs
@@ -1,83 +1,40 @@
 export const resume = {
 
-    name: {
-        fName: "Sauel",
-        lName: "A"
-    },
-
-    location: {
-        city: "Boston",
-        state: "MA"
-    },
-
-    
-    schools: {
-        from: "New England Art Institute",
-        certificate: "Audio Engineering, Mixing Engineering, & Mastering Engineering"
-        
-
-        // {
-        //     name: "Cybersounds Studio",
-        //     certificate: "Ear Trainning, Audio Production, Mic Placement"
-        // },
-
-        // {
-        //     name: "Berklee College of Music",
-        //     certificate: "Audio Engineering Advanced, Mixing Engineering Advanced, Mastering Engineering Advanced"
-        // },
-
-        // {
-        //     name: "ULEM MSIMBO",
-        //     certificate: "MSIMBO Coding"
-        // },
-
-    },
-
-    
-    
-    interests: {
-        book: "How To Buy Diamonds",
-        author: "Fred Cuellar",
-            
-        
-        // {
-        //     book: "The Richest Man In Babylon ",
-        //     author: "George S. Clason"
-        // },
-
-        // {    
-        //     book: "Life and Def",
-        //     author: "Russell Simmons"
-        // },        
-
-        
-
-        sports: [
-            {
-                team: "Celtics",
-                from: "Boston, MA"
-            },
-            
-            {
-                team: "Red Sox",
-                from: "Boston, MA"
-            },
-
-            {
-                team: "Patriots",
-                from: "New England"
-            },
-
-        ],
-
-        music: {
-            1: "Hip-Hop",
-            2: "R&B",
-            3: "Reggae",
-            4: "Salsa",
-            5: "Merengue"    
+    "details" : {
+        "firstName" : "Sauel",
+        "lastName" : "Almonte",
+        "contact" : {
+            "number" : "857-000-0000",
+            "email" : "salmonte@msimbo.org",
         },
+        "location" : {
+            "city" : "Boston",
+            "state" : "MA",
+        },
+        "education" : {
+            "school1" : "New England Art Institute",
+            "certificate1" : "Audio Engineering, Mixing Engineering, & Mastering Engineering",
 
+            "school2" : "Cybersounds Studio",
+            "certificate2" : "Ear Trainning, Audio Production, & Mic Placement",
+
+            "school3" : "Berklee College of Music",
+            "certificate3" : "Audio Engineering Advanced, Mixing Engineering Advanced, & Mastering Engineering Advanced",
+        },
+        "interests" : {
+            "reading" : {
+                "book1" : "How To Buy Diamonds",
+                "author1" : "Fred Cuellar",
+                "book2" : "The Richest Man In Babylon",
+                "author2" : "George S. Clason",
+                "book3" : "Life and Def",
+                "author3" : "Russell Simmons",    
+            },  
+            "sports" : {
+                "basketball" : "Boston Celtics",
+                "baseball" : "Boston Red Sox",
+                "football" : "New England Patriots",  
+            },
+        },
     },
-
 }
