aa8232f3a6ed349ebe7f01567bfcb993d163c7e2
msimbo-coding <SAlmonte@MSIMBO.org>
2021-02-11 11:06:15 -0500

    updating server.mjs and package.json

--- server.mjs
+++ server.mjs
@@ -9,12 +9,12 @@ const PORT = process.env.PORT || 5000;
 // route
 
 
-// shortened format
-app.get('/resume', (req, res) => res.send(resume) );
-
 app.get('/', (req,res) => {
     res.send(`Welcome to my resume! Please run command: [ npx i resume-app ] to access my resume. Please visit my /bio endpoint`)
 });
+
+// shortened format
+app.get('/resume-app', (req, res) => res.send(resume) );
 
 app.listen( PORT, () => {
     console.log(`Hello and thank you for visiting my resume on ${PORT}`)
